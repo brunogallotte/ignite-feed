911ffcb9c9e97825d8d04599cf0b2ddf42f75f6a
Bruno Gallotte <brunogallotte1@gmail.com>
2023-12-12 20:00:55 -0300

    Feat: added sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { Header } from './components/Header.jsx'
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar.jsx'
 
 export function App() {
 
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h2>teste</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat libero aperiam incidunt quia nostrum, dolorem quam assumenda, fuga aliquid quo molestiae accusamus similique blanditiis beatae iure placeat quae necessitatibus illo.</p>
+        </main>
+      </div>
+    </div>
   )
 }
