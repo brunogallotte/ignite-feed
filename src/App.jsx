59f247322a8fdec0f47fe91f2e279f8514719c3a
Bruno Gallotte <brunogallotte1@gmail.com>
2023-12-13 15:03:52 -0300

    Feat: added post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from './components/Header.jsx'
+import { Post } from './components/Post.jsx'
+import { Sidebar } from './components/Sidebar.jsx'
 
 import styles from './App.module.css'
 
 import './global.css'
-import { Sidebar } from './components/Sidebar.jsx'
 
 export function App() {
 
@@ -14,8 +15,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <h2>teste</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat libero aperiam incidunt quia nostrum, dolorem quam assumenda, fuga aliquid quo molestiae accusamus similique blanditiis beatae iure placeat quae necessitatibus illo.</p>
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
